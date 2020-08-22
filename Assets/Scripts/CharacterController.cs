457490086d766d5a5334740ee6a2973d86566574
Daniel Stotts <dstodev@gmail.com>
2020-08-22 11:58:38 -0700

    Camera follows player

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,37 +5,43 @@ using UnityEngine.EventSystems;
 
 public class CharacterController : MonoBehaviour
 {
-    [Range(1, 20)]
-    public float Speed = 1f; // tiles per second (or something idk)
+	[Range(1, 20)]
+	public float Speed = 1f; // tiles per second (or something idk)
 
-    Camera cam;
+	Camera cam;
 
-    public void Start()
-    {
-        cam = Camera.main;
-    }
+	public void Start()
+	{
+		cam = Camera.main;
+	}
 
-    public void Update()
-    {
-        Vector3 vel = new Vector3();
-        if (Input.GetKey("w"))
-        {
-            vel.y += 1;
-        }
-        if (Input.GetKey("s"))
-        {
-            vel.y -= 1;
-        }
-        if (Input.GetKey("a"))
-         {
-            vel.x -= 1;
-        }
-        if (Input.GetKey("d"))
-         {
-            vel.x += 1;
-         }
+	public void Update()
+	{
+		Vector3 vel = new Vector3();
+		if (Input.GetKey("w"))
+		{
+			vel.y += 1;
+		}
+		if (Input.GetKey("s"))
+		{
+			vel.y -= 1;
+		}
+		if (Input.GetKey("a"))
+		{
+			vel.x -= 1;
+		}
+		if (Input.GetKey("d"))
+		{
+			vel.x += 1;
+		}
 
-        vel = vel.normalized * Speed;
-         GetComponent<Transform>().position += vel * Time.deltaTime;
-    }
+		Transform transform = GetComponent<Transform>();
+		Transform camTransform = cam.GetComponent<Transform>();
+
+		Vector3 pos = new Vector3(transform.position.x, transform.position.y, camTransform.position.z);
+		camTransform.position = pos;
+
+		vel = vel.normalized * Speed;
+		transform.position += vel * Time.deltaTime;
+	}
 }
