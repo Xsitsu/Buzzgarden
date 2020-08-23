6bc6a32482a17975a2b1531a9c74d1c2e3503cb8
Xsitsu <jacoblocke21@live.com>
2020-08-23 02:20:22 -0700

    Made basic game UI that displays current pollen count.

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI PollenText;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        int pollen = PlayerInventory.Instance.Pollen;
+        if (pollen >= 1000000)
+        {
+            float num = ((float)(pollen / 100000)) / 10;
+            PollenText.text = num.ToString() + "M";
+        }
+        else if (pollen >= 10000)
+        {
+            float num = ((float)(pollen / 1000));
+            PollenText.text = num.ToString() + "K";
+        }
+        else if (pollen >= 1000)
+        {
+            float num = ((float)(pollen / 100)) / 10;
+            PollenText.text = num.ToString() + "K";
+        }
+        else
+        {
+            PollenText.text = pollen.ToString();
+        }
+    }
+}
