362d0cb5e023f54ea21f3504bdd7d619fedb4b24
Xsitsu <jacoblocke21@live.com>
2020-08-25 15:21:26 -0700

    Fixed bug with pollen harvesting sound. Sound will now be played and stopped correctly.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -13,10 +13,12 @@ public class CharacterController : MonoBehaviour
 	public float PollenHarvestDistance = 1f; // tiles
 
 	Camera cam;
+	bool wasHarvesting;
 
 	void Start()
 	{
 		cam = Camera.main;
+		wasHarvesting = false;
 		transform.localPosition = new Vector3(MapHandler.Instance.MapSize.x / 2, MapHandler.Instance.MapSize.y / 2, 0);
 	}
 
@@ -54,57 +56,60 @@ public class CharacterController : MonoBehaviour
 		vel = vel.normalized * Speed;
 		transform.position += vel * Time.deltaTime;
 
-		if (true || Input.GetMouseButton(0))
+		//Debug.Log("Pos: " + transform.localPosition.x + "/" + transform.localPosition.y);
+		float posX = transform.localPosition.x;
+		float posY = transform.localPosition.y;
+		int floorX = (int)Mathf.Floor(posX);
+		int floorY = (int)Mathf.Floor(posY);
+		float decX = (posX - floorX);
+		float decY = (posY - floorY);
+
+		int coordX = floorX;
+		int coordY = floorY;
+
+		if (decX >= 0.5f)
 		{
-			//Debug.Log("Pos: " + transform.localPosition.x + "/" + transform.localPosition.y);
-			float posX = transform.localPosition.x;
-			float posY = transform.localPosition.y;
-			int floorX = (int)Mathf.Floor(posX);
-			int floorY = (int)Mathf.Floor(posY);
-			float decX = (posX - floorX);
-			float decY = (posY - floorY);
+			coordX++;
+		}
+		if (decY >= 0.5f)
+		{
+			coordY++;
+		}
 
-			int coordX = floorX;
-			int coordY = floorY;
+		//int coordX = (int)(transform.position.x + 0.5f);
+		//int coordY = (int)(transform.position.y + 0.5f);
 
-			if (decX >= 0.5f)
+		Vector3Int cell = new Vector3Int(coordX, coordY, 0);
+		//Vector3Int cell = TileSelector.Instance.SelectedCell;
+
+		Vector2 diff = new Vector2((float)cell.x - transform.position.x, (float)cell.y - transform.position.y);
+		float dist = diff.magnitude;
+
+		bool isHarvesting = false;
+		MapTile tile = MapHandler.Instance.Map.GetTileFromCell(cell);
+		if (dist <= PollenHarvestDistance)
+		{
+			if (tile != null && tile.Flower != null)
 			{
-				coordX++;
-			}
-			if (decY >= 0.5f)
-			{
-				coordY++;
-			}
-
-			//int coordX = (int)(transform.position.x + 0.5f);
-			//int coordY = (int)(transform.position.y + 0.5f);
-
-			Vector3Int cell = new Vector3Int(coordX, coordY, 0);
-			//Vector3Int cell = TileSelector.Instance.SelectedCell;
-
-			Vector2 diff = new Vector2((float)cell.x - transform.position.x, (float)cell.y - transform.position.y);
-			float dist = diff.magnitude;
-
-			MapTile tile = MapHandler.Instance.Map.GetTileFromCell(cell);
-			if (dist <= PollenHarvestDistance)
-			{
-				if (tile != null && tile.Flower != null)
+				tile.Flower.SetParticlesTarget(gameObject);
+				FlowerType flowerType = tile.Flower.flowerType;
+				float harvested = tile.Flower.HarvestPollen(PollenHarvestSpeed * Time.deltaTime);
+				PlayerInventory.Instance.AddPollen(flowerType.Id, harvested);
+				if (harvested > 0)
 				{
-					tile.Flower.SetParticlesTarget(gameObject);
-					FlowerType flowerType = tile.Flower.flowerType;
-					float harvested = tile.Flower.HarvestPollen(PollenHarvestSpeed * Time.deltaTime);
-					PlayerInventory.Instance.AddPollen(flowerType.Id, harvested);
-					//tile.Flower.flowerType.PollenCollected += harvested;
-					if (harvested > 0)
-					{
-						SoundManager.Instance.PlayGetPollen();
-					}
-					else
-					{
-						SoundManager.Instance.StopSound();
-					}
+					isHarvesting = true;
 				}
 			}
 		}
+
+		if (!wasHarvesting && isHarvesting)
+		{
+			SoundManager.Instance.PlayGetPollen();
+		}
+		else if (wasHarvesting && !isHarvesting)
+		{
+			SoundManager.Instance.StopSound();
+		}
+		wasHarvesting = isHarvesting;
 	}
 }
